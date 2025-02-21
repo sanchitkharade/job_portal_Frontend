17c0888a8f326590522be61abe9b8a64776dd5cb
Sanchit <sanchitkharade1120@gmail.com>
2025-02-21 14:43:05 +0530

    asch

--- src/Components/PostJobs/PostJob.tsx
+++ src/Components/PostJobs/PostJob.tsx
@@ -7,8 +7,10 @@ import { isNotEmpty, useForm } from "@mantine/form";
 import { postJob } from "../../Services/JobService";
 import { errorNotification, successNotification } from "../../Services/NotificationService";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const PostJob=()=>{
+    const profile = useSelector((state:any)=>state.profile);
     const navigate=useNavigate();
     const select=fields;
     const form=useForm({
@@ -41,9 +43,18 @@ const PostJob=()=>{
     const handlePost=()=>{
         form.validate();
         if(!form.isValid())return;
-        postJob(form.getValues()).then((res)=>{
+        postJob({...form.getValues(), postedBy:profile.id,jobStatus:"ACTIVE"}).then((res)=>{
             successNotification("Success","Job Posted Successfully");
-            navigate("/posted-job");
+            navigate(`/posted-job/${res.id}`);
+        }).catch((err)=>{
+            console.log(err);
+            errorNotification("Error",err.response.data.errorMessage);
+        });
+    }
+    const handleDraft=()=>{
+        postJob({...form.getValues(), postedBy:profile.id,jobStatus:"DRAFT"}).then((res)=>{
+            successNotification("Success","Job Saved to Drafts Successfully");
+            navigate(`/posted-job/${res.id}`);
         }).catch((err)=>{
             console.log(err);
             errorNotification("Error",err.response.data.errorMessage);
@@ -72,7 +83,7 @@ const PostJob=()=>{
             </div>
             <div className="flex gap-4">
             <Button color="brightSun.4" onClick={handlePost} variant="light">Publish Job</Button>
-            <Button color="brightSun.4" variant="outline">Save as Draft</Button>
+            <Button color="brightSun.4" onClick={handleDraft} variant="outline">Save as Draft</Button>
             </div>
         </div>
     </div>
