9e206d3bf43b20fa4a3f41390648ae887522ef66
Sanchit <sanchitkharade1120@gmail.com>
2025-03-31 17:06:37 +0530

    1

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://job-portal-backend-2-pgt4.onrender.com/',
     
 })
 
